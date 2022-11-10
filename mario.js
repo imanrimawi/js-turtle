43c8767a2531de1b7a19b4635fcefa66f7f59108
“imanrimawi” <rimawi.26@acsamman.edu.jo>
2022-11-10 09:32:28 +0300

    changes-mario

--- mario.js
+++ mario.js
@@ -1,129 +1,28 @@
-var min = -getWidth()/2;
-var max =  getHeight()/2;
-
-
+var minX  = -getWidth()/2
+var minY  =  -getHeight()/2
+var maxY  =  getHeight()/2
+var maxX  =  getWidth()/2
+    
 function main()
 {
-    //set up and move to starting position
-    wrap(false);
-    
-    goto(min, min);
+   right(200);
+   forward(20);
+   forward(10);
+   left(180);
+   forward(30);
+   left(200);
+   forward(30);
+   right(180);
+   forward(30);
+   forward(20);
+   right(90);
+   forward(20);
+   left(180);
+   forward(40);
+   right(180);
+   forward(20);
+   left(90);
+   forward(10);
+   shape("circle");
 
-    //get numver of bricks to draw from user
-    numSquares = prompt("number of bricks: ");
-
-    //calculate the side length
-    sideLength = getWidth()/numSquares;
-
-    for (var i = 0; i < numSquares; i++)
-    {
-        brickBlock(sideLength);
-        strafe(sideLength);
-    }
-
-}
-
-function rectangle(height, width) {
-    for(var i = 0; i < 4; i++){
-        if (i % 2 == 0)
-        {
-            forward(height);
-        }
-        else
-        {
-            forward(width);
-        }
-       right(90);
-    }
- }
-
-function brickBlock(sideLength)
-{
-    brickRow(sideLength, 2)
-    penup();
-    forward(sideLength/3)
-    pendown();
-    brickRow(sideLength, 3)
-    penup();
-    forward(sideLength/3)
-    pendown();
-    brickRow(sideLength, 2)
-    penup()
-    left(180);
-    forward(sideLength*2/3)
-    left(180);
-
-
-}
-
-function brickRow(sideLength, numBricks)
-{
-    if (numBricks == 2)
-    {
-        rectangle(sideLength/3, sideLength/2);
-        strafe(sideLength/2)
-        rectangle(sideLength/3, sideLength/2);
-        strafe(-sideLength/2)
-    }
-
-    if (numBricks == 3)
-    {
-        penup();
-        forward(sideLength/3);
-        pendown();
-        right(90);
-        forward(sideLength/4);
-        right(90)
-        forward(sideLength/3);
-        right(90);
-        forward(sideLength/4);
-        right(90);
-        strafe(sideLength/4)
-        rectangle(sideLength/3, sideLength/2);
-        strafe(sideLength/2)
-        
-        forward(sideLength/3);
-        right(90);
-        forward(sideLength/4);
-        right(90)
-        penup();
-        forward(sideLength/3);
-        pendown();
-        right(90);
-        forward(sideLength/4);
-        right(90);
-
-
-        strafe(-sideLength*3/4);
-
-    }
-
-}
-
-
-
-
-function strafe(distance)
-{
-    penup();
-    if (distance < 0)
-    {
-        left(90);
-        forward(Math.abs(distance));
-        right(90);
-    }
-    else
-    {
-        right(90);
-        forward(Math.abs(distance));
-        left(90);
-    }
-    pendown();
-
-}
-
-
-function main()
-{
-    
 }
--- myscript.js
+++ myscript.js
@@ -5,7 +5,24 @@ var maxX  =  getWidth()/2
     
 function main()
 {
-    
+   right(200);
+   forward(20);
+   forward(10);
+   left(180);
+   forward(30);
+   left(200);
+   forward(30);
+   right(180);
+   forward(30);
+   forward(20);
+   right(90);
+   forward(20);
+   left(180);
+   forward(40);
+   right(180);
+   forward(20);
+   left(90);
+   forward(10);
+   shape("circle");
+
 }
-
-
